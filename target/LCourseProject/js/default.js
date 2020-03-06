1325050cc6fa905bf6170298fa4dc8f077970870
vzakharchenya <vadzim_zakharchenya@epam.com>
2020-03-06 23:14:17 +0300

    clean up

--- target/LCourseProject/js/default.js
+++
@@ -1,8 +0,0 @@
-function refreshProtection(event) {
-    if (event.key === "F5"
-        || event.keyCode === 116) {
-        event.preventDefault();
-        event.stopPropagation();
-    }
-}
-window.addEventListener("keydown", refreshProtection);
